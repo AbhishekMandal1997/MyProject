5d93c4a2fd3888fc105a8a84eddcf836351adcaa
Abhishek Mandal <abhishekabk1997@gmail.com>
2024-06-29 22:46:45 +0530

    Abhishek

---
+++ Day2Problem1.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter num1: "))
+print(num1)
+# def UpdateAndCompare(num1, num2, num3):
+#     if num1 > num2:
+#         print("true")
+#     else:
+#         print("false")
+#         num1 += num3  # Update num1 by adding num3
+#     if num1 > num2:
+#         print("true")
+#     else:
+#         print("false")
+
+# Example usage:
+
+# UpdateAndCompare(num1, num2, num3)
